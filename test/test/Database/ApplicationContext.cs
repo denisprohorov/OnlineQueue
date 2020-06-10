2d4979f073c0d07ec9dc117667f4603458c2f8c9
denisprohorov <denprohorov8@yandex.com>
2020-06-10 16:36:57 +0300

    Requered for Cherry-pick

--- test/test/Database/ApplicationContext.cs
+++ test/test/Database/ApplicationContext.cs
@@ -1,15 +1,1 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace test.Database
-{
-    public class ApplicationContext : IdentityDbContext<UserDbModel>
-    {
-        public DbSet<QueueDbModel> Queues { get; set; }
-        public ApplicationContext(DbContextOptions<ApplicationContext> options)
-            : base(options)
-        {
-            Database.EnsureCreated();
-        }
-    }
-}
+﻿
--- test/test/Database/UserDbModel.cs
+++ test/test/Database/UserDbModel.cs
@@ -1,16 +1,1 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-
-namespace test.Database
-{
-    public class UserDbModel : IdentityUser
-    {
-        public List<int> QueuesAsMember { get;set;}
-        public List<int> QueuesAsTeacher { get;set;}
-        public List<int> QueuesAsOuthor { get;set;}
-
-        public List<int> FollowingQueues{get;set;}
-        public List<string> FollowingUsers{get;set;}
-        public List<string> Followers{get; set;}
-    }
-}
+﻿
